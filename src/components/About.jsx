d46fc0a86c1d93930db0f5069464c539fde59a9d
Pranav <pranav@Pranavs-MacBook-Air.local>
2023-10-22 21:48:29 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="About"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20 max-sm:mt-10">
+          Hello, I am K.Sai Pranav pursuing my BTech in VIT-AP in the stream of
+          Computer Science and Engineering. I am passionate about coding and
+          problem solving and found my interest in web and application
+          development. I have prior knowledge on front end web technologies like
+          HTML, CSS,javascript and learning MERN stack.
+        </p>
+        <br />
+        <p>
+          While coming to my academics, I have an aggregate CGPA of 9.04 for my
+          whole 3 years of engineering.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
